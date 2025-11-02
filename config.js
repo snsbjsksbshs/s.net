b058229e3e23bdf06984b4b794fdcf75b7ee2c9a
Al-Sarq Net <mamerbsher@gmail.com>
2025-11-02 19:11:36 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,74 @@
+﻿Config({
+    "network-name": "السرق نت ",
+    "service-number": "770979313رقم الهاتف",
+    "login-type": "passwordAsUser",
+    "news-line": "...:::: اهلاً بكم على شبكة السرق نت اللاسلكية تتميز شبكتنا بتقديم مزايا مختلفة لمشتركيها الاعزاء منها السرعة في التصفح ولعب الألعاب والإستراحة الشاملة والبث المباشر للمباريات ايضاً نقدم خدمة توصيل الانترنت الى المنازل لسنا الوحيدون ولكن نسعى لتقديم خدمة ممتازة على مدار 24 ساعة::::...",
+    "input-type": "text",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-text-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "redirect-to-esterahah": "", //<---إذا عندك إستراحة اكتب او الصق الرابط داخل علامتين التنصيص الذي قبل هذه العلامتين
+    "redirect-to-mobasher": "", //<---إذا عندك بث مباشر اكتب او الصق الرابط داخل علامتين التنصيص الذي قبل هذه العلامتين
+    "app-store-base-url": "", // <---إذا عندك متجر تطبيقات اكتب او الصق الرابط داخل علامتين التنصيص الذي قبل  هذه العلامتين
+    "profiles": [{
+            "price": "100 ريال",
+            "time": "6 ساعات",
+            "transfer": "250 ميجا",
+            "validity": "1 ايام",
+        },
+        {
+            "price": "250 ريال",
+            "time": "20 ساعات",
+            "transfer": "700 ميجا",
+            "validity": "4 ايام",
+        },
+        {
+            "price": "500 ريال",
+            "time": "30 ساعة",
+            "transfer": "1500 ميجا",
+            "validity": "7 ايام",
+        },
+        {
+            "price": "1000 ريال",
+            "time": "اسبوع",
+            "transfer": "3 حيجا",
+            "validity": "15 ايام",
+        },
+        {
+            "price": "3000 ريال",
+            "time": "30 يوم",
+            "transfer": "9 جيجا",
+            "validity": "30 يوم",
+        },
+     
+    
+     
+
+
+    ],
+    "sell-points": [
+        { "name": "قرية معبره بقالة ابو وضاح المقداد" },
+        { "name": "قرية العقاير مختار سعيد" },
+        { "name": "فجرة بقالة ايمن الورافي" },
+       
+    ]
+})
